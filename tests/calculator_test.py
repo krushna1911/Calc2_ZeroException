960af8ed3220f44a02f03da2ee3aa3270f9fab84
krushna1911 <krushna1911@yahoo.com>
2021-11-02 03:16:33 -0400

    corrected code

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -33,5 +33,5 @@ def test_calculator_multiply():
 def test_calculator_divide():
     """ tests multiplication of two numbers"""
     calc = Calculator()
-    result = calc.divide_numbers(1, 0)
+    result = calc.divide_numbers(1, 2)
     assert result == 0.5
